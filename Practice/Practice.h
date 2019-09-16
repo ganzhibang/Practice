e5c29ef400cd492c10b35b85e19d355d4a98983f
2017yd <2017yd@lzzydeiMac.local>
2019-09-16 08:14:13 +0800

    Initial Commit

---
+++ Practice/Practice.h
@@ -0,0 +1,19 @@
+//
+//  Practice.h
+//  Practice
+//
+//  Created by 2017yd on 2019/9/16.
+//  Copyright © 2019年 2017yd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Practice.
+FOUNDATION_EXPORT double PracticeVersionNumber;
+
+//! Project version string for Practice.
+FOUNDATION_EXPORT const unsigned char PracticeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Practice/PublicHeader.h>
+
+
